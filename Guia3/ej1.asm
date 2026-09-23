8cbdc33385e57de479b29d326bf5a0c09ac3493e
Seider <frannvallone@gmail.com>
2026-09-23 14:25:55 -0300

    Guia3: ej1 arreglado

--- Guia3/ej1.asm
+++ Guia3/ej1.asm
@@ -2,8 +2,8 @@
 ; que se ingresan por teclado.
 ; El fin de la lista se indica con un número negativo.
 
-INICIO: XOR EFX, EFX    ;SETEAR EN 0 EL CONTADOR i=0
-        XOR AC, AC    ;SETEAR EN 0 LA SUMA sum=0
+IINICIO: XOR EFX, EFX    ; contador i = 0
+        XOR EBX, EBX    ; suma = 0
 
 LEE:    MOV EAX, 1
         MOV EDX, DS
@@ -11,28 +11,25 @@ LEE:    MOV EAX, 1
         LDH ECX, 4
         SYS 1
 
-CMP:    MOV EAX, [EDX]  ;COPIO EL CONTENIDO DE EDX, QUE SERA EL NUMERO LEIDO EN EAX
-        CMP EAX, -1
-        JZ FIN  ;SI ES -1 TERMINO
-        JN LEE  ;SI ES NEGATIVO LEO OTRA VEZ Y NO HAGO NADA CON LOS OTROS
+VERIFICA:
+        MOV EAX, [EDX]
         CMP EAX, 0
-        JZ LEE  ;SI ES CERO, LEO
+        JN FIN          ; cualquier negativo corta la lista
+        JZ LEE          ; el 0 no cuenta, sigo leyendo
 
-;SI LLEGO ESTA ACA ESTA TODO OK
-ALGO:   ADD [EFX], 1    ;i++
-        ADD AC, EAX    ;sum+=num
+ALGO:   ADD EFX, 1      ; i++
+        ADD EBX, EAX    ; suma += num
         JMP LEE
 
-FIN:    ADD EDX, 4
-        CMP EFX, 0
+FIN:    CMP EFX, 0
         JZ ERROR
-        DIV AC, EFX ;sum/i
-        MOV [EDX], AC
+        DIV EBX, EFX    ; ebx = suma / i (el resto va a AC, no me importa)
+        ADD EDX, 4
+        MOV [EDX], EBX  ; guardo el promedio en la celda 4
+
 MUESTRA: MOV EAX, 1
-        MOV EDX, DS
         LDH ECX, 4
         LDL ECX, 1
-        SYS 2
+        SYS 2           ; edx ya apunta a donde está el promedio
 
 ERROR:  STOP
-
